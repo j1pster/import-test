fc1458e50b2e61e42082ab26301d675e2755b669
Jip Leijdekkers <jip@dasbanner.com>
2023-06-22 14:13:37 +0200

    [Docker][Database] - create migrations, add docker

---
+++ database/migrations/2023_06_22_080557_create_records_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('account')->nullable();
+            $table->string('address')->nullable();
+            $table->timestamp('birthday')->nullable();
+            $table->boolean('checked')->default(false);
+            $table->string('email')->nullable();
+            $table->interest('interest')->nullable();
+            $table->string('phone')->nullable();
+            
+            $table->string('credit_card_number')->nullable();
+            $table->string('credit_card_type')->nullable();
+            $table->string('credit_card_name')->nullable();
+            $table->string('credit_card_expiration_date')->nullable();
+
+            $table->unsignedBigInteger('import_id')->references('id')->on('imports');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('records');
+    }
+};
